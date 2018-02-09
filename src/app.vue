4cc1e677203afd874876411cdf6a4d23dde26816
yangjing <yangjingpuyu@aliyun.com>
2018-02-09 17:27:39 +0800

    start

---
+++ src/app.vue
@@ -0,0 +1,21 @@
+<template lang="html">
+  <div class="" id="text">
+    {{text}}
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      text: 'abc'
+    }
+  }
+}
+</script>
+
+<style lang="css">
+  #text{
+    color: blue
+  }
+</style>
